762ff71c899b43821967ec8387f7cdd753ee1ed8
Josh Kramer <josh@joshkra.me>
2017-08-21 17:26:10 -0400

    Add Entry component

---
+++ src/app/entry/entry.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entry',
+  templateUrl: './entry.component.html',
+  styleUrls: ['./entry.component.css']
+})
+export class EntryComponent implements OnInit {
+  public entry: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.entry = {
+      date: '2017-04-05',
+      gallons: 10.888,
+      odometer: 15444
+    }
+  }
+
+}
