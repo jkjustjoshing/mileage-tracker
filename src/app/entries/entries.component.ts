b45f8db9a4005728960b4656ecaa481cb0e8c0af
Josh Kramer <josh@joshkra.me>
2017-08-24 11:13:20 -0400

    Get totalGallons working with Firebase data.

--- src/app/entries/entries.component.ts
+++ src/app/entries/entries.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Entry } from './entry.model';
 import { EntriesService } from './entries.service';
 
 import { FirebaseListObservable } from 'angularfire2/database';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-entries',
   templateUrl: './entries.component.html',
   styleUrls: ['./entries.component.scss']
 })
-export class EntriesComponent implements OnInit {
+export class EntriesComponent implements OnInit, OnDestroy {
   entries: FirebaseListObservable<Entry[]>;
   totalGallons: number;
+  subscription: Subscription;
 
 
   constructor(private entriesService: EntriesService) {}
 
   ngOnInit() {
     this.entries = this.entriesService.getEntries();
-    this.entriesService.totalGallons.subscribe(gallons => {
+    this.subscription = this.entriesService.totalGallons.subscribe(gallons => {
       this.totalGallons = gallons;
     });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   onEntryCreated(entry: Entry) {
