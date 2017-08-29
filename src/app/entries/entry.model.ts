050b2b022058cdebaf9d25c0d6e3a76ca9ad8bd5
Josh Kramer <jkjustjoshing@gmail.com>
2017-08-29 12:32:06 -0400

    Add totalPrice to Entry and change styling.

--- src/app/entries/entry.model.ts
+++ src/app/entries/entry.model.ts
@@ -2,14 +2,16 @@ export class Entry {
   date: string;
   odometer: number;
   gallons: number;
+  totalPrice: number;
 
-  constructor ({date, odometer, gallons}: Entry = <Entry> {}) {
+  constructor ({date, odometer, gallons, totalPrice}: Entry = <Entry> {}) {
     this.date = date;
     this.odometer = odometer;
     this.gallons = gallons;
+    this.totalPrice = totalPrice;
   }
 
   isEmpty() {
-    return !this.date || !this.odometer || !this.gallons;
+    return !this.date || !this.odometer || !this.gallons || !this.totalPrice;
   }
 }
