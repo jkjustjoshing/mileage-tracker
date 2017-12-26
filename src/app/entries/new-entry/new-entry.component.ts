dbb270e4dc72826bb78f29e192d0c99f6f9de40b
Josh Kramer <jkjustjoshing@gmail.com>
2017-12-26 17:05:15 -0500

    Remove bootstrap, further implement material design.

--- src/app/entries/new-entry/new-entry.component.ts
+++ src/app/entries/new-entry/new-entry.component.ts
@@ -1,30 +1,23 @@
 import { EntriesService } from './../entries.service';
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { Entry, EntryInput } from '../entry.model';
-import { calendar } from 'octicons';
 
 @Component({
   selector: 'app-new-entry',
   templateUrl: './new-entry.component.html',
   styleUrls: ['./new-entry.component.scss']
 })
-export class NewEntryComponent implements OnInit {
+export class NewEntryComponent {
   @Output()
   entryCreated = new EventEmitter<Entry>();
 
   entry: EntryInput = <EntryInput> {};
-  calendarIcon: SafeHtml;
 
-  constructor(private sanitizer: DomSanitizer,
+  constructor(
     private route: ActivatedRoute,
     private entriesService: EntriesService,
     private router: Router) { }
-
-  ngOnInit() {
-    this.calendarIcon = this.sanitizer.bypassSecurityTrustHtml(calendar.toSVG());
-  }
 
   createEntry(entry: EntryInput) {
     const uid = this.route.snapshot.params.uid;
